c55a5ac4dfa4fdb22e2aef8105b1cadc8dfbdedb
Allana Evellyn <allanaevellynm@gmail.com>
2022-01-04 23:15:54 -0300

    created modal component and its states and handles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+
 import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [newTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  function handleOpenTransactionModal() {
+    setNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenTransactionModal}/>
       <Dashboard />
-      <GlobalStyle /> 
+      <NewTransactionModal isOpen={newTransactionModalOpen} onRequestClose={handleCloseTransactionModal}/>
+      <GlobalStyle />
     </>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,25 @@
-import logoImg from '../../assets/logo.png';
-import { Container, Content } from './styles';
+import logoImg from "../../assets/logo.png";
+import Modal from "react-modal";
+import { Container, Content } from "./styles";
 
-export function Header() {
-    return (
-        <Container>
-            <Content>
-                <div className='app-title'>
-                    <img src={logoImg} alt="dt money" />
-                    <h1 >b* better have my money</h1>
-                </div>
-                <button type="button">
-                    Nova transação
-                </button>
-            </Content>
-        </Container>
-    )
-};
+interface HeaderProps {
+  onOpenTransactionModal: () => void;
+}
+
+export function Header({onOpenTransactionModal}: HeaderProps) {
+  
+
+  return (
+    <Container>
+      <Content>
+        <div className="app-title">
+          <img src={logoImg} alt="dt money" />
+          <h1>b* better have my money</h1>
+        </div>
+        <button type="button" onClick={onOpenTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+}
